117beac5751d00f462426c432e9d4758308fe0ff
jielyh <hyper_lil_angel_905@hotmail.com>
2012-01-20 10:05:21 -0500

    added files

---
+++ js/functions.js
@@ -0,0 +1,64 @@
+// javascript single line comment
+/*
+Javascript multi-line comment
+*/
+
+// 'alert()' pops up a little window with okay button
+alert('This is an alert.');
+
+// 'prompt()' is for users, has a text field for the user to enter some text
+var i;
+while (!i) {
+i = prompt('Type in a letter');
+}
+
+var x = 1;
+
+while ( x <= 10 ) {
+document.write('<br>' + i);
+x++;
+} 
+
+while ( x <= 5 ) {
+document.write('<br>' + i);
+x++;
+} 
+//tace() and consol.log() = same thing
+//'console.log()' writes outs the content of a variable for debugging
+// identical to Actionscript's 'trace()'
+console.log (i);
+
+//'document.write()' allows us to output stuff to the end of the HTML file
+// the '+' operator is for combining strings and together
+//document.write('<br>' + i);
+
+
+// (i) is a variable we made to determine the number of loops
+//for (var i = 0; i < 1; i++) {
+	//document.write('<br>' + i);
+	//document.write('<br>' + i + i);
+	//document.write('<br>' + i + i+ i);
+	//document.write('<br>' + i + i + i + i);
+	//document.write('<br>' + i + i + i + i + i);
+	//document.write('<br>' + i + i + i + i + i + i);
+	//document.write('<br>' + i + i + i + i + i + i + i);
+	//document.write('<br>' + i + i + i + i + i + i + i + i);
+	//document.write('<br>' + i + i + i + i + i + i + i + i + i);
+	//document.write('<br>' + i + i + i + i + i + i + i + i + i + i); //<br> = new line for every + new number
+	//document.write('<h1>' + i + '<h1>');
+//}
+
+
+
+//isNameCool(); //making the function
+
+//var writeNameMultipleTimes = function () {
+//	for (var i = 0; i < 5; i++ ) {
+	//	document.write('<strong> Jiely </strong> <br>');
+	//}
+//};
+
+//writeNameMultipleTimes ();
+
+
+
